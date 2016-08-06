22d1db92a51a1ca1eb97f4879f87951d9757b399
maaron <m_aaron@yahoo.com>
2016-08-06 17:24:50 -0500

    F# version is almost level with C#, except for scrollviewer persistence

--- FSharp/Program.fs
+++ FSharp/Program.fs
@@ -1,7 +1,12 @@
-﻿open System
+﻿module TodoElmStyle
+
+open System
 open System.Windows
 open System.Windows.Controls
 open System.Windows.Controls.Primitives
+open System.Windows.Shapes
+open System.Windows.Media
+open XamlBuilders
 
 type Task = {
     Text: String;
@@ -27,13 +32,113 @@ let update (msg: Msg) (model: Todos): Todos =
     | Complete (task, complete) -> 
         let completeTask t = if t = task then { t with Completed = complete } else t
         { model with Tasks = List.map completeTask model.Tasks }
+
+let headerView (model: Todos) dispatch =
+    Grid() {
+        columns [
+            ColumnDefinition(Width = GridLength.Auto)
+            ColumnDefinition()
+        ]
+        children [
+            TextBlock(Text = "Add a todo") {
+                set [Grid.ColumnProperty, 0]
+            }
+            TextBox() {
+                set [Grid.ColumnProperty, 1]
+
+                onLoaded (fun t e -> t.Focus())
+
+                onKeyDown (fun t e -> 
+                    if e.Key = Input.Key.Enter then
+                        e.Handled <- true
+                        Add { Text = t.Text; Completed = false } |> dispatch)
+            }
+        ]
+    }
+
+let taskView (model: Task) dispatch =
+    StackPanel(Orientation = Orientation.Horizontal) {
+        children [
+            CheckBox(
+                Margin = Thickness(5.0), 
+                IsChecked = Nullable<bool>(model.Completed)
+            ) {
+                onClick (fun c e -> Complete (model, not model.Completed) |> dispatch)
+            }
+            TextBlock(
+                Margin = Thickness(5.0),
+                Text = model.Text,
+                TextDecorations = if model.Completed 
+                    then TextDecorations.Strikethrough
+                    else null
+            )
+            Button(
+                Margin = Thickness(5.0),
+                HorizontalContentAlignment = HorizontalAlignment.Center,
+                VerticalContentAlignment = VerticalAlignment.Center,
+                Content = Grid() {
+                    children [
+                        Line(
+                            Stroke = Brushes.Red,
+                            StrokeThickness = 2.0,
+                            X1 = 0.0, Y1 = 0.0,
+                            X2 = 10.0, Y2 = 10.0
+                        )
+                        Line(
+                            StrokeThickness = 2.0,
+                            Stroke = Brushes.Red,
+                            X1 = 0.0, Y1 = 10.0,
+                            X2 = 10.0, Y2 = 0.0
+                        )
+                    ]
+                }
+            ) {
+                onClick (fun c e -> Remove model |> dispatch)
+            }
+        ]
+    } :> UIElement
+
+let bodyView (model: Todos) dispatch =
+    StackPanel(Orientation = Orientation.Vertical) {
+        children (Seq.map (fun t -> taskView t dispatch) model.Tasks)
+    }
+
+let footerView (model: Todos) dispatch =
+    StackPanel(Orientation = Orientation.Horizontal) {
+        children [
+            TextBlock(Text = "Show Completed")
+            CheckBox()
+        ]
+    }
     
-let view (model: Todos): UIElement =
-    Grid()
+let view (model: Todos) dispatch =
+    Grid(Margin = Thickness(5.0)) {
+        rows [
+            RowDefinition(Height = GridLength.Auto)
+            RowDefinition()
+            RowDefinition(Height = GridLength.Auto)
+        ]
+
+        children [
+            headerView model dispatch { set [Grid.RowProperty, 0] }
+            bodyView model dispatch { set [Grid.RowProperty, 1] }
+            footerView model dispatch { set [Grid.RowProperty, 2] }
+        ]
+    }
+
+let initialState: Todos = { Tasks = []; Filter = true }
 
 [<STAThread>]
 [<EntryPoint>]
 let main argv = 
     let mainWindow = Window()
+    let mutable state = initialState
+    
+    let rec dispatch msg =
+        state <- update msg state
+        mainWindow.Content <- view state dispatch
+
+    mainWindow.Content <- view state dispatch
+    
     let application = Application()
     application.Run(mainWindow)
