91db274d93318ec3065ff3f00430c7b267dc54ac
maaron <maaron@yahoo.com>
2017-03-22 16:57:19 -0500

    Seem to have got padded and margined layout combinators working.

--- FSharp/observable.fsx
+++ FSharp/observable.fsx
@@ -12,13 +12,7 @@
 #r "../packages/FSharp.Control.Reactive.3.5.0/lib/net45/FSharp.Control.Reactive.dll"
 
 open FSharp.Control.Reactive
-
-type Size = 
-  { width: float
-    height: float }
-
-    with
-        static member zero = { width = 0.0; height = 0.0 }
+open System.Numerics
 
 type Point =
   { x: float
@@ -26,10 +20,13 @@ type Point =
     
     with
         static member zero = { x = 0.0; y = 0.0 }
+        static member square x = { x = x; y = x }
+        static member add p1 p2 = { x = p1.x + p2.x; y = p1.y + p2.y }
+        static member subtract p1 p2 = { x = p1.x - p2.x; y = p1.y - p2.y }
 
 type Rectangle =
   { topLeft: Point
-    size: Size }
+    size: Point }
 
 type Color = int
 
@@ -40,7 +37,18 @@ type Stroke =
   { brush: Brush
     thickness: float }
 
-type Matrix3x2 = class end
+type Matrix3x2 =
+  { M11: float; M21: float; M31: float
+    M12: float; M22: float; M32: float }
+
+    with
+        static member Translation p =
+          { M11 = 1.0; M21 = 0.0; M31 = p.x
+            M12 = 0.0; M22 = 1.0; M32 = p.y }
+
+        static member TransformPoint m p = 
+          { x = m.M11 * p.x + m.M21 * p.y + m.M31
+            y = m.M12 * p.x + m.M22 * p.y + m.M32 }
 
 type Drawing = 
     | DrawEmpty
@@ -52,8 +60,17 @@ type Drawing =
     | DrawGroup of Drawing list
 
 type View =
-  { size: Size
+  { size: Point
     drawing: Drawing }
+
+    with
+        static member pad width { size = size; drawing = drawing } = 
+          { size = { x = size.x + width * 2.0; y = size.y + width * 2.0 }
+            drawing = DrawTransformed (Matrix3x2.Translation { x = width; y = width }, drawing) }
+
+        static member margin newsize width { size = size; drawing = drawing } = 
+          { size = newsize
+            drawing = DrawTransformed (Matrix3x2.Translation (Point.square width), drawing) }
 
 let view size drawing = { size = size; drawing = drawing }
 
@@ -64,31 +81,70 @@ type Mouse =
 
 type UIEvent<'e> =
     | MouseEvent of Mouse
-    | WindowEvent of Size
+    | WindowEvent of Point
     | Event of 'e
 
-type UI<'e, 'c, 'a> =
-  { input: System.IObserver<UIEvent<'e>>
-    output: System.IObservable<'a>
-    commands: System.IObservable<'c>
+type UI<'i, 'o> =
+  { input: System.IObserver<UIEvent<'i>>
+    output: System.IObservable<'o>
     view: System.IObservable<View> }
 
 let empty() =
   { input = System.Reactive.Observer.Create(System.Action<_>(ignore))
     output = Observable.single ()
-    commands = Observable.empty
-    view = Observable.single (view { width = 0.0; height = 0.0 } DrawEmpty) }
+    view = Observable.single (view Point.zero DrawEmpty) }
 
 let map f ui = 
   { input = ui.input
     output = ui.output |> Observable.map f 
-    commands = ui.commands 
     view = ui.view }
+
+let loop ui =
+    ui.output.Subscribe(ui.input) |> ignore
+    ui
+
+let chooseWindowEvents input = 
+    input
+    |> Observable.choose (function WindowEvent e -> Some e | _ -> None)
+
+type ViewTransform =
+  { twindow: Point -> Point
+    tmouse: Point -> Point
+    tview: Point -> View -> View }
+
+let selectWindow = function WindowEvent e -> Some e | _ -> None
+
+// This combinator is deceptively difficult.
+// MouseEvent -> Invert -> Forward to UI
+// WindowEvent -> Limit -> Forward to UI
+// UI View -> Transform -> Output View
+let arranged transform (ui: UI<'a, 'b>): UI<'a, 'b> =
+    let input = System.Reactive.Subjects.Subject ()
+
+    let mapped =
+        input
+        |> Observable.map (function
+            | MouseEvent e -> 
+                MouseEvent { e with location = transform.tmouse e.location } 
+            | WindowEvent e ->
+                WindowEvent (e |> transform.twindow)
+            | _ as e -> e)
+
+    mapped.Subscribe (ui.input) |> ignore
+
+    let windowEvents = input |> Observable.choose selectWindow
+    
+    let output = ui.output
+    let view = 
+        ui.view 
+        |> Observable.combineLatest windowEvents
+        |> Observable.map (fun (size, view) -> transform.tview size view)
+
+    { input = input; output = output; view = view }
 
 let bindView f ui =
   { input = ui.input
     output = ui.output
-    commands = ui.commands
     view = ui.view |> Observable.merge (ui.output |> Observable.map f) }
 
 let chooseEvent f ui =
@@ -103,28 +159,27 @@ let chooseEvent f ui =
     
     { input = input :> System.IObserver<_>
       output = output
-      commands = ui.commands
       view = ui.view }
 
-let mouse (ui: UI<'e, 'c, 'a>): UI<'e, 'c, 'a * Mouse> =
+let mouse (ui: UI<'e, 'a>): UI<'e, 'a * Mouse> =
     chooseEvent (function MouseEvent e -> Some e | _ -> None) ui
 
-let window (ui: UI<'e, 'c, 'a>): UI<'e, 'c, 'a * Size> =
+let window (ui: UI<'e, 'a>): UI<'e, 'a * Point> =
     chooseEvent (function WindowEvent e -> Some e | _ -> None) ui
 
 let staticView view = { empty () with view = Observable.single view }
 
 let fill rectangle brush = 
     let bounds = 
-      { width = rectangle.topLeft.x + rectangle.size.width
-        height = rectangle.topLeft.y + rectangle.size.height }
+      { x = rectangle.topLeft.x + rectangle.size.x
+        y = rectangle.topLeft.y + rectangle.size.y }
 
     staticView <| view bounds (DrawFill (rectangle, brush))
 
 let rectangle rectangle brush = 
     let bounds = 
-      { width = rectangle.topLeft.x + rectangle.size.width
-        height = rectangle.topLeft.y + rectangle.size.height }
+      { x = rectangle.topLeft.x + rectangle.size.x
+        y = rectangle.topLeft.y + rectangle.size.y }
 
     staticView <| view bounds (DrawRectangle (rectangle, brush))
 
@@ -136,21 +191,46 @@ let clear brush =
         view size (DrawFill (bounds, brush)))
     |> map ignore
 
-let myui: UI<unit, unit, _> = 
+let padded width ui =
+    let tform = 
+      { twindow = id
+        tmouse = Point.subtract { x = width; y = width }
+        tview = fun size view -> View.pad width view }
+    arranged tform ui
+
+let margined width ui =
+    let tform = 
+      { twindow = fun size -> 
+            Point.subtract size (Point.square (width * 2.0))
+        tmouse = Point.subtract (Point.square width)
+        tview = fun size view -> View.margin size width view }
+    arranged tform ui
+
+let myui (): UI<unit, _> = 
     clear (Solid 123)
+    (*
     |> mouse
     |> bindView (fun (_, m) -> 
         view Size.zero (DrawText <| string m.location))
+    *)
+    |> padded 10.0
 
 let run ui events =
-    ui.view.Subscribe (fun view -> printf "%A\n" view) |> ignore
+    ui.view.Subscribe (fun view -> printf "UI event: %A\n" view) |> ignore
     events |> List.iter (fun e ->
-        ui.input.OnNext (e))
+        ui.input.OnNext (e))            
 
-run myui 
-  [ WindowEvent { width = 200.0; height = 400.0 }
+run (myui ())
+  [ WindowEvent { x = 200.0; y = 400.0 }
+    (*
     MouseEvent
       { location = { x = 1.0; y = 10.0 }
         leftButton = false
-        rightButton = false } ]
+        rightButton = false } *)
+        ]
 
+run (myui()) []
+run (clear (Solid 1) |> padded 5.0) [ WindowEvent { x = 200.0; y = 400.0 } ]
+run (clear (Solid 1) |> margined 5.0) [ WindowEvent { x = 200.0; y = 400.0 } ]
+View.pad 1.0 (view { x = 5.0; y = 5.0 } (DrawFill ({topLeft = Point.zero; size = { x = 5.0; y = 5.0 }}, Solid 2)))
+View.margin { x = 2.0; y = 2.0 } 1.0 (view { x = 5.0; y = 5.0 } (DrawFill ({topLeft = Point.zero; size = { x = 5.0; y = 5.0 }}, Solid 2)))
